11f1aa6c1a192c890cc95eabb068e5f27034f8ba
Gummiryala Srikanth <gummiryalasrikanth369@gmail.com>
2024-11-04 18:04:33 +0530

    Student Management Portal

---
+++ Assignment-8/src/Components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <>
+      <nav className="bg-blue-300">
+        <div className="d-flex justify-content-center gap-4 p-3">
+          <Link to="/" className="text-decoration-none text-light active">
+            Search
+          </Link>
+          <Link
+            to="/addYourDetails"
+            className="text-decoration-none text-light"
+          >
+            Add Details
+          </Link>
+          <Link
+            to="/listOfStudents"
+            className="text-decoration-none text-light"
+          >
+            List of Students
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+};
+export default Navbar;
